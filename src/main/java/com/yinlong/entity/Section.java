b49802fceff54fa0d28e4f98e9719569d84b0292
WorldTooBig <994497455@qq.com>
2018-05-25 17:15:15 +0800

    first

---
+++ src/main/java/com/yinlong/entity/Section.java
@@ -0,0 +1,81 @@
+package com.yinlong.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ * ฟฦสา
+ * @author 01270059
+ *
+ */
+@Entity
+@Table(name = "YL_SECTION")
+@SuppressWarnings("serial")
+public class Section implements Serializable {
+
+	private int sectId;
+	private String sectName;
+	private String sectRemark;
+	
+	private Department dept;
+
+	public Section() {
+		super();
+	}
+
+	public Section(int sectId, String sectName, String sectRemark) {
+		super();
+		this.sectId = sectId;
+		this.sectName = sectName;
+		this.sectRemark = sectRemark;
+	}
+
+	@Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQ_SECTION")   
+    @SequenceGenerator(name="SEQ_SECTION", sequenceName="SEQ_SECTION",allocationSize = 1)  
+	public int getSectId() {
+		return sectId;
+	}
+
+	public void setSectId(int sectId) {
+		this.sectId = sectId;
+	}
+
+	public String getSectName() {
+		return sectName;
+	}
+
+	public void setSectName(String sectName) {
+		this.sectName = sectName;
+	}
+
+	public String getSectRemark() {
+		return sectRemark;
+	}
+
+	public void setSectRemark(String sectRemark) {
+		this.sectRemark = sectRemark;
+	}
+
+	@ManyToOne(cascade=CascadeType.MERGE,fetch=FetchType.EAGER)
+	@JoinColumn(name="deptId")
+	public Department getDept() {
+		return dept;
+	}
+
+	public void setDept(Department dept) {
+		this.dept = dept;
+	}
+	
+	
+}
---
+++ src/main/java/com/yinlong/filter/LoginFilter.java
@@ -0,0 +1,48 @@
+package com.yinlong.filter;
+
+import com.yinlong.entity.User;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class LoginFilter implements Filter {
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// 获取request,response,session对象
+		HttpServletRequest servletRequest = (HttpServletRequest) request;
+		HttpServletResponse servletResponse = (HttpServletResponse) response;
+		HttpSession session = servletRequest.getSession();
+
+		// 获取用户请求的URI
+		String path = servletRequest.getRequestURI();
+
+		// 获取session中用户名信息
+		User user = (User) session.getAttribute("user_login");
+		if(user!=null)
+			chain.doFilter(request, response);
+
+		// 判断是否为登陆界面，如果是登陆界面则放行，否则进行用户信息检查
+		if(path.indexOf("/login.jsp") >-1 || path.contains("userLogin")) {
+			chain.doFilter(request, response);
+		}else if(!path.contains(".jsp")&&(path.contains(".css")||path.contains(".js")||path.contains(".ttf")||path.contains(".ico")||path.contains("Mobile"))) {
+			chain.doFilter(request, response);
+		}else {
+			if(user == null) {
+				servletResponse.sendRedirect("/AfterServiceStstem/pages/login.jsp");
+				return;
+			}
+		}
+
+	}
+
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+
+}
